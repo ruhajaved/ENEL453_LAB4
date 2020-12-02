4b8a1317c46f681301ea6fa8a7e31a053c3e291d
ruhajaved <72583753+ruhajaved@users.noreply.github.com>
2020-12-02 00:25:21 -0700

    Add files via upload

--- PWM_DAC2.vhd
+++ PWM_DAC2.vhd
@@ -3,49 +3,49 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
 entity PWM_DAC2 is
-   --Generic ( width : integer := 9);
+   generic ( width : integer := 9);
    Port    ( reset_n    : in  STD_LOGIC;
 			    EN		: in STD_LOGIC; -- used to slow down counter to make flashing visible to human eye
              clk        : in  STD_LOGIC;
              --duty_cycle : in  STD_LOGIC_VECTOR (width-1 downto 0);
-			    dc_width : in STD_LOGIC_VECTOR (3 downto 0);
+			 --dc_width : in STD_LOGIC_VECTOR (3 downto 0);
              pwm_out    : out STD_LOGIC
            );
 end PWM_DAC2;
 
 architecture Behavioral of PWM_DAC2 is
 
-signal N : integer;
-signal counter_max : integer;--unsigned (width_c-1 downto 0);
-signal counter : integer;
-signal duty_cycle : integer;
+--signal N : natural;
+-- signal counter_max : integer; -- range 0 to (2**N-1);--unsigned (width_c-1 downto 0);
+-- signal counter : integer;
+-- signal duty_cycle : integer;
 --signal temp : unsigned (width_c-1 downto 0);
+
+signal counter : unsigned (width-1 downto 0);
+signal temp : unsigned (width-1 downto 0) := (others => '1');
+signal duty_cycle : unsigned (width-1 downto 0) := temp srl 1;
        
 begin
 	--width_c <= to_integer(unsigned(dc_width));
 	--temp <= (others => '1');
 	--duty_cycle <= temp srl 1; -- duty cylce will be half of the max counter value
 	
-	N <= to_integer(unsigned(dc_width));
-	counter_max <= 2**N-1;
-	duty_cycle <= 2**(N-1)-1;
+	--N <= to_integer(unsigned(dc_width));
+	--counter_max <= 2**N-1;
+	--duty_cycle <= counter_max * (1/2);
 
    count : process(clk, reset_n)
    begin
        if( reset_n = '0') then
-           counter <= 0;--(others => '0');
+           counter <= (others => '0');--(others => '0');
        elsif (rising_edge(clk) and EN = '1') then 
-				if (counter < counter_max) then
-					counter <= counter + 1;
-				else
-					counter <= 0;
-				end if;
+		   counter <= counter + 1;
        end if;
    end process;
  
    compare : process(counter, duty_cycle)
    begin    
-       if (counter <= duty_cycle) then
+       if (counter <= duty_cycle) then -- how does the equal sign give a metastable value?
            pwm_out <= '0';
        else 
            pwm_out <= '1';
--- downcounter.vhd
+++ downcounter.vhd
@@ -5,10 +5,11 @@ use IEEE.math_real.ceil;
 use IEEE.math_real.log2;
 
 entity downcounter is
-    Generic ( period  : natural := 6000000); -- number to count       
+    --Generic ( period  : natural := 5); -- number to count    					-- change back to 6 000000   
     PORT    ( clk     : in  STD_LOGIC; -- clock to be divided
               reset_n : in  STD_LOGIC; -- active-high reset
               enable  : in  STD_LOGIC; -- active-high enable
+			  distance : in STD_LOGIC_VECTOR(12 downto 0); -- added to choose width
               zero    : out STD_LOGIC  -- creates a positive pulse every time current_count hits zero
                                        -- useful to enable another device, like to slow down a counter
               -- value  : out STD_LOGIC_VECTOR(integer(ceil(log2(real(period)))) - 1 downto 0) -- outputs the current_count value, if needed
@@ -16,9 +17,36 @@ entity downcounter is
 end downcounter;
 
 architecture Behavioral of downcounter is
-   signal current_count : natural;
+
+   signal current_count : integer;
+   signal period : integer;
+   signal distance_t : unsigned(12 downto 0);
+   
+   -- with distance select period <=
+		-- 6_000_000 when (distance > 1500 and distance < 2000),
+		-- 5_500_000 when (distance > 1000 and distance < 1500),
+		-- 5_000_000 when (distance > 500 and distance < 1000),
+		-- 4_500_000 else 
+
   
 BEGIN
+
+	distance_t <= unsigned(distance);
+	
+	select_period : process(distance)
+		begin
+		if (distance_t < 500) then -- can you do this comparison?
+			period <= 4; --45_000;
+		elsif (distance_t < 1000) then
+			period <= 5; --50_000;
+		elsif (distance_t < 1500) then
+			period <= 6; --50_500;
+		else --if (distance_t < 2000) then should work for anything above 2000 and if anything is above 2000 it'll just get blocked later on
+			period <= 7; --60_000;	
+		--else
+		--	period <= ; -- check if this can't be zero
+		end if;
+		end process;
    
    downcount : process(clk,reset_n) begin
      if (reset_n = '0') then 
